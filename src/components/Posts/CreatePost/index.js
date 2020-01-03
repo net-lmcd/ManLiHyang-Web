0ea129bd9cbbeeae8bd6beb2affb9c550eed5158
doonguk <mbxd1@naver.com>
2020-01-03 18:10:09 +0900

    글 작성 헤더

--- src/components/Posts/CreatePost/index.js
+++ src/components/Posts/CreatePost/index.js
@@ -3,19 +3,23 @@ import { Link } from "react-router-dom"
 import { useSelector } from "react-redux"
 import './index.scss'
 
+// save 버튼 글자 없으면 색깔 다름
+
 const CreatePost = props => {
   const { history, dispatch } = props
-  const { thumbnail } = useSelector(state => state.books)
+  const { thumbnail, bookName } = useSelector(state => state.books)
+
 
   return(
       <div className="create-wrap">
         <img src={thumbnail} alt="" className="background-thumbnail"/>
         <div className="gray-zone">
           <div className="btn-section">
-            <Link to="/post/search" className="cancel-btn">&times;</Link>
-            <span>0/500</span>
-            <span>Save</span>
+             <img src="/assets/white-cancel.png" alt=""/>
+            <span className="contents-length">0/500</span>
+            <span className="save-btn">Save</span>
           </div>
+          <div className="book-title">{bookName}</div>
         </div>
       </div>
   )
